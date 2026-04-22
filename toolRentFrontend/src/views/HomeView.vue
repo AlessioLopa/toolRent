3272e410adca80df20fd178dcacbdd916f771941
Alessio Lopardo <alessio.lopardo@protonmail.com>
2026-04-22 16:01:01 +0200

    feat: implements homeview with tools listing, timesimulation and delayed tools

--- toolRentFrontend/src/views/HomeView.vue
+++ toolRentFrontend/src/views/HomeView.vue
@@ -1,5 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ToolsListComponent from '@/components/ToolsListComponent.vue'
+import { getTools, getToolsOverdue } from '@/services/toolsService'
+import { onMounted, ref } from 'vue'
+
+const tools = ref()
+
+onMounted(async () => {
+  tools.value = await getAllTools()
+})
+
+const getAllTools = async () => {
+  try {
+    tools.value = await getTools()
+  } catch (error: any) {
+    alert('Failed to fetch tools: ' + error.message)
+  }
+}
+
+const toolsOverdue = async () => {
+  try {
+    tools.value = await getToolsOverdue()
+  } catch (error: any) {
+    alert('Failed to fetch overdue tools: ' + error.message)
+  }
+}
+const timeSimulation = () => {}
+</script>
 
 <template>
-  <main></main>
+  <div class="page">
+    <div class="list-options">
+      <Button @click="getAllTools()">See all tools</Button>
+      <button @click="toolsOverdue()">See tools overdue</button>
+      <button @click="timeSimulation()">Simulate time (+48h)</button>
+    </div>
+    <div class="data-table">
+      <ToolsListComponent :tools="tools" />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.page {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  width: 100%;
+}
+
+.list-options {
+  display: flex;
+  gap: 1rem;
+  margin-right: 2rem;
+}
+</style>
