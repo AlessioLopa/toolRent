77b3367c34790d21c475694dd6d58a8f37784f60
AlessioLopa <alessio.lopardo@protonmail.com>
2026-04-24 15:43:32 +0200

    feat: move Bearer token setting to request interceptor for better authorization handling

--- toolRentFrontend/src/services/apiService.ts
+++ toolRentFrontend/src/services/apiService.ts
@@ -5,6 +5,22 @@ export const api = axios.create({
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('token')}`,
   },
 })
+
+// Set the Bearer token for all requests
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+
+    return config
+  },
+
+  (error) => {
+    return Promise.reject(error)
+  }
+)
